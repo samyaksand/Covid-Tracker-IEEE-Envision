79ec5c62062312855da1e66926538b9f16c7779a
Samyak Sand <samyaksand@gmail.com>
2021-05-11 20:21:48 +0400

    Updated repo with API and CSS part

---
+++ app.py
@@ -0,0 +1,21 @@
+import requests, json
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+# API - to access web data
+
+url = 'https://www.mohfw.gov.in/data/datanew.json' 
+
+#perform GET request
+res = requests.get(url)
+
+#convert data into json
+data = res.json()
+
+@app.route('/')
+def home_page():
+    return render_template('index.html', data = data)
+
+if __name__=="__main__":
+    app.run(debug=True)
