20cd9c01ca9f26842dd9cea2280bb32b07d5e8c4
SHASHANK SK <its.shashank.sk@gmail.com>
2021-05-13 19:11:02 +0530

    changes

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
+import requests
+import json
 from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-# web scarping...
+# API - to access web data
 
-data = {
+url = 'https://www.mohfw.gov.in/data/datanew.json'
 
-    'total': 300,
-    'active': 200,
-    'recover': 100,
+# perform GET request
+res = requests.get(url)
 
-}
+# convert data into json
+data = res.json()
+
 
 @app.route('/')
 def home_page():
-    return render_template('index.html', data = data)
+    return render_template('index.html', data=data)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
