264d8cc27e62bc00ee0b5d409aaa850e079da4bf
SHASHANK SK <its.shashank.sk@gmail.com>
2021-05-20 17:41:33 +0530

    graph n map in map i got and error no module request

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import requests
 import json
 import pygal
 from flask import Flask, render_template
+#import pandarequest
+import panda as pd
+import plotly.express as px
 
 app = Flask(__name__)
 
@@ -20,11 +23,16 @@ data = res.json()
 plot_TC = pygal.Bar()
 plot_new_dead =  pygal.Bar()
 plot_cured = pygal.Bar()
+plot_newpositive = pygal.Bar()
 
 
 plot_TC.title = 'total cases'
 plot_new_dead.tile = 'NEW DEATHS'
 plot_cured.title = 'CURED'
+plot_newpositive.title = 'NEW POSITIVE'
+
+
+
 
 # basically sort the data in acending order
 sorted_data = sorted(data, key=lambda x: int(x['new_active']))
@@ -32,6 +40,10 @@ sorted_data = sorted(data, key=lambda x: int(x['new_active']))
 sorted_data_death = sorted(data, key=lambda x: int(x['new_death']))  #for the deaths
 
 sorted_data_cured = sorted(data, key=lambda x: int(x['cured'])) #for cured
+
+sorted_data_newpositive = sorted(data, key = lambda x :int(x['new_positive']))#new positive
+
+
 
 
 
@@ -71,7 +83,6 @@ plot_new_dead.render_to_file('static/data/active_ded_long.svg')
 
 
 
-
 #this is for the template tile========================================================
 for dict in sorted_data_cured[1:11]:
     plot_cured. add (dict['state_name'], int(dict['cured']))
@@ -90,6 +101,50 @@ plot_cured.render_to_file('static/data/active_cured_long.svg')
 
 
 
+
+#this is for the template tile========================================================
+for dict in sorted_data_newpositive[1:11]:
+    plot_newpositive. add (dict['state_name'], int(dict['new_positive']))
+
+plot_newpositive.render_to_file('static/data/positive.svg')
+#======================================================================================
+
+#for separate web page CURED cases=====================================================
+for dict in sorted_data_newpositive[1:25]:
+    plot_newpositive.add(dict['state_name'], int(dict['new_positive']))
+
+plot_newpositive.render_to_file('static/data/positive_long.svg')
+#======================================================================================
+
+
+#maps::================================================================================
+
+data.pop()
+df = pd.DataFrame(data)
+df['active'] = df['active'].astype(int)
+
+geojson_file = open('india.geojson','r')
+geojson_file = json.load(geojson_file)
+
+map = px.choropleth(
+    df,
+    locations = 'state_name',
+
+    color = 'active',
+
+    geojson = geojson_data,
+    featureidkey = 'properties.ST_NM',
+
+    color_continuous_scale = 'red'
+)
+
+map.update_geos(fitbounds = 'locations', visible = false)
+
+map.write_html('static/data/map.html')
+
+#======================================================================================
+
+
 @app.route('/')
 def home_page():
     return render_template('index p1.html', data=data)
@@ -98,7 +153,18 @@ def home_page():
 def dead_graph():
     return render_template('dead_graph.html')
 
-    
+@app.route('/graph')
+def graph():
+    return render_template('graph.html')
+ 
+@app.route('/cured')
+def cured():
+    return render_template('cured.html')
+
+@app.route('/newpositive')
+def newpositive():
+    return render_template('newpositive.html')
+
 
 
 if __name__ == "__main__":
